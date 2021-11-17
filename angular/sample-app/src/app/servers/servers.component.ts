951028b239e15ed42777fa5999fc7c76582d81de
Konstantsin Shchuchkin <K.Shchuchkin@itechart-group.com>
2021-11-17 18:27:16 +0300

    feat: Add ngStyle ngClass etc. dirs for excersize

--- angular/sample-app/src/app/servers/servers.component.ts
+++ angular/sample-app/src/app/servers/servers.component.ts
@@ -2,13 +2,57 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: ` <app-server></app-server>
-    <app-server></app-server>
-    <app-server></app-server>`,
-  styleUrls: ['./servers.component.css'],
+  templateUrl: './servers.component.html',
+  styles: [
+    `
+      .fifthLog {
+        color: white;
+      }
+    `,
+  ],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  // allowNewServer: boolean = false;
+  // isCreated: boolean = false;
+  // serverCreationStatus: string = 'No new server was created!';
+  // serverName: string = 'Test';
+  // username: string = '';
+  // servers: string[] = ['testServer', 'anotherTestServer'];
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.allowNewServer = true;
+  //   }, 2000);
+  // }
+  // ngOnInit(): void {}
+  // onCreateServer() {
+  //   this.isCreated = true;
+  //   this.servers.push(this.serverName);
+  //   this.serverCreationStatus = `New server was created! ${this.serverName}`;
+  // }
+  // onUpdateServerName(event: Event) {
+  //   console.log(event);
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+  // }
+  // onUpdateUsername() {
+  //   this.username = '';
+  // }
+  isToggled: boolean = false;
+  buttonLogs: string[] = [];
 
   ngOnInit(): void {}
+
+  constructor() {}
+
+  onDisplayDetails() {
+    this.isToggled = !this.isToggled;
+    this.buttonLogs.push(`Button is clicked at ${new Date()}`);
+  }
+
+  getColor() {
+    return this.isToggled ? '' : 'white';
+  }
+
+  getLogColor() {
+    return this.buttonLogs.length > 4 ? 'blue' : '';
+  }
 }
